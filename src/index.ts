80136a295099abdd0cb4430069d8b64e7faa01d6
Andrei Muntean <account@andreimuntean.dev>
2023-04-09 21:20:28 +0300

    Add CORS header

--- src/index.ts
+++ src/index.ts
@@ -7,9 +7,7 @@ const app: Express = express();
 
 async function getLanguage(lang: string = "en") {
   const url = `https://wol.jw.org/${lang}/wol/li/r34/lp-m`;
-  const data = await axios(url, {
-    headers: { "Access-Control-Allow-Origin": "*" },
-  }).then((res) => res.data);
+  const data = await axios(url).then((res) => res.data);
 
   const $ = load(data);
   const language = $("#wrapper > #regionMain > #content > #article > #libraryNav > .completeList").find(`ul > li > a[data-locale=${lang}]`).attr('data-meps-symbol');
@@ -22,9 +20,7 @@ app.get('/', async (req: Request, res: Response) => {
   const lang = await getLanguage(req.query.lang as string);
   if (!lang) res.status(400).send("The language you requested does not exist. Please try again using a supported language.");
   const url = `https://www.jw.org/finder?srcid=jwlshare&wtlocale=${lang}&alias=daily-text&date=${date}`;
-  const rawData = await axios(url, {
-    headers: { "Access-Control-Allow-Origin": "*" },
-  }).then((res) => res.data);
+  const rawData = await axios(url).then((res) => res.data);
 
   const $ = load(rawData);
   const text = $(".dailyText .articlePositioner").find(".tabContent:nth-child(2) > p.themeScrp > em").text().replaceAll(" ", "").replace(" ()", "");
@@ -32,6 +28,7 @@ app.get('/', async (req: Request, res: Response) => {
   const comentary = $(".dailyText .articlePositioner").find(".tabContent:nth-child(2) > .bodyTxt > .section > .pGroup > p.sb").text().split(". w")[0] + ".";
   const comentarySource = `https://wol.jw.org${$(".dailyText .articlePositioner").find(".tabContent:nth-child(2) > .bodyTxt > .section > .pGroup > p.sb > a:last-of-type").attr('href')}`;
 
+  res.setHeader("Access-Control-Allow-Origin", "*")
   res.statusCode = 200;
   res.send(JSON.stringify({ verse, text, comentary, comentarySource, url }));
 });
